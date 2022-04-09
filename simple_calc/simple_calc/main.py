9fe63960d1e6479067052cfd2cb085602e897099
Eriksson Santos <erikssonsantos.py@gmail.com>
2022-04-09 19:11:24 -0300

    simple_calc minificado

--- simple_calc/simple_calc/main.py
+++ simple_calc/simple_calc/main.py
@@ -1,12 +1,85 @@
 from kivymd.app import MDApp
-from mixinmain import MixinMain
-import rootwidget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+
+
+kv_0 = """
+<RootWidget>:
+    
+    size_hint: 1, 1
+    orientation: 'vertical'
+
+    MDBoxLayout:
+        size_hint: 1, 1
+        orientation: 'vertical'
+        padding: [20,20,20,20]
+        spacing: 10
+
+        MDLabel:
+            id: id_resultado
+            text: ''
+            size: self.texture_size
+            text_size: self.size
+            halign: 'center'
+            valign: 'center'
+
+        MDTextField:
+            hint_text: "Quantidade de aulas"
+            mode: "rectangle"
+            id: id_aulas
+        MDTextField:
+            hint_text: "Quantidade de faltas"
+            mode: "rectangle"
+            id: id_faltas
+
+        MDBoxLayout:
+            size_hint: 1, 1
+            orientation: 'horizontal'
+            MDBoxLayout:
+            MDRaisedButton:
+                text: 'Calcular'
+                on_release: root.simple_calc()
+            MDBoxLayout:
+"""
+
+
+class MixinRootWidget(object):
+
+    def simple_calc(self):
+
+        aulas = self.ids.id_aulas.text
+        faltas = self.ids.id_faltas.text
+        
+        try:
+            aulas = int(aulas)
+            faltas = int(faltas)
+            if faltas > aulas:
+                self.ids.id_resultado.text = "Valores não aceitáveis."
+            else:
+                self.ids.id_resultado.text = f'Aluno com {int(100 - (faltas / aulas * 100))}% de frequência.'
+        except:
+            self.ids.id_resultado.text = "Valores não aceitáveis."    
+
+
+class RootWidget(MixinRootWidget, BoxLayout):
+    ...
+
+
+class MixinMain(object):
+    ...
 
 
 class MainApp(MDApp, MixinMain):
-
+    
+    def __init__(self, **kwargs):
+        super(MainApp, self).__init__(**kwargs)
+        
+        Builder.load_string(kv_0)
+        self.root_widget = RootWidget()
+        self.theme_cls.primary_palette = "DeepPurple"
+    
     def build(self):
-        return rootwidget.RootWidget()
+        return RootWidget()
 
 
 if __name__ == '__main__':
