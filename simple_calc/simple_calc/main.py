31f4858922615e9ea9dd1efe14603bd29ac33f07
Eriksson Santos <erikssonsantos.py@gmail.com>
2022-04-11 19:32:21 -0300

    Update main.py

--- simple_calc/simple_calc/main.py
+++ simple_calc/simple_calc/main.py
@@ -1,53 +1,76 @@
 from kivymd.app import MDApp
-from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.floatlayout import MDFloatLayout
 from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.config import Config
+
+
+Config.set("kivy", "keyboard_mode", 'dock')
 
 
 kv_0 = """
+#:import utils kivy.utils
+
+
 <RootWidget>:
     
-    size_hint: 1, 1
+    # size_hint: 1, 1
     orientation: 'vertical'
-
-    MDLabel:
-        size_hint: 1, .6
-        id: id_resultado
-        text: ''
-        size: self.texture_size
-        text_size: self.size
-        halign: 'center'
-        valign: 'center'
-
-    MDBoxLayout:
-        size_hint: 1, 1
-        orientation: 'vertical'
-        padding: [20,2,20,2]
-        spacing: 1
-
+    
+    
+    StackLayout:
+        # orientation: 'tb-lr'
+        pos_hint: {'center_x': .5, 'y': .6}
+        size_hint: .8, .6
+        spacing: 5
+        
+        
+        MDLabel:
+            # pos_hint: {'center_x': .5, 'center_y': .2}
+            # size_hint: 1, 1
+            # minimum_height: 40
+            # size_hint_x: 1
+            id: id_resultado
+            text: ''
+            bold: True
+            font_size: '35dp'
+            size: self.texture_size
+            text_size: self.size
+            halign: 'center'
+            valign: 'middle'
+            markup: True
+            #canvas.before:
+            #    Color:
+            #        rgba: utils.get_color_from_hex('#FEFEFE')
+            #    Rectangle:
+            #        size: self.width, self.height
+            #        pos: self.x, self.y
+        
         MDTextFieldCustom:
             hint_text: "Quantidade de aulas"
             id: id_aulas
         MDTextFieldCustom:
             hint_text: "Quantidade de faltas"
             id: id_faltas
+        MDRaisedButton:
+            text: 'Calcular'
+            on_release: root.simple_calc()
+            font_size: '30dp'
+            # size_hint_x: 1
+            # size_hint_y: 1
 
-        MDBoxLayout:
-            size_hint: 1, 1
-            orientation: 'horizontal'
-            MDBoxLayout:
-            MDRaisedButton:
-                text: 'Calcular'
-                on_release: root.simple_calc()
-            MDBoxLayout:
-    MDBoxLayout:
-        size_hint: 1, .8
 
 <MDTextFieldCustom@MDTextField>:
     mode: "rectangle"
-    #size_hint_y: None
-    #height: 44
     max_text_length: 2
-
+    input_filter: 'int'
+    font_size: '40dp'
+    halign: 'center'
+    required: False
+    helper_text_mode: "on_error"
+    helper_text: ""
+    multiline: False
+    input_type: 'number'
 """
 
 
@@ -64,12 +87,17 @@ class MixinRootWidget(object):
             if faltas > aulas:
                 self.ids.id_resultado.text = "Valores não aceitáveis."
             else:
-                self.ids.id_resultado.text = f'Aluno com {int(100 - (faltas / aulas * 100))}% de frequência.'
+                infrequendia = faltas / aulas * 100
+                frequencia = int(100 - infrequendia)
+                if frequencia < 75:
+                    self.ids.id_resultado.text = f'Aluno com [color=FF0000]{frequencia}%[/color] de presença.'
+                else:
+                    self.ids.id_resultado.text = f'Aluno com [color=026A55]{frequencia}%[/color] de presença.'
         except:
             self.ids.id_resultado.text = "Valores não aceitáveis."    
 
 
-class RootWidget(MixinRootWidget, BoxLayout):
+class RootWidget(MixinRootWidget, MDFloatLayout):
     pass
 
 
@@ -78,6 +106,8 @@ class MixinMain(object):
 
 
 class MainApp(MDApp, MixinMain):
+    
+    Window.softinput_mode = "below_target" # ['', 'below_target', 'pan', 'scale', 'resize'] comportamento do teclado do android
     
     def __init__(self, **kwargs):
         super(MainApp, self).__init__(**kwargs)
